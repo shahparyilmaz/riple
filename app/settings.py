b3dc34b396f3c1daed2cde74480adcb1b4e8a1b7
shahparyilmaz <shahpar09@gmail.com>
2021-03-04 04:17:17 +0530

    .

--- app/settings.py
+++ app/settings.py
@@ -124,12 +124,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL="home"
 MEDIA_URL='/images/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'images')
+
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
